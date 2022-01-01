dfcb0ed1e495ef2589b74ef8d5ae888524658dea
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-01-01 17:50:19 +0100

    Modification affichage html du VueRender et ajout de l'html erreur

--- src/view/VueRender.php
+++ src/view/VueRender.php
@@ -34,18 +34,22 @@ class VueRender
         <!DOCTYPE html>
         <html>
             <head>
-                <title>My Wish List</title>
+                <title>MyWishList</title>
+            <link rel="stylesheet" href="{$this->container->router->pathFor('accueil')}css/style.css" type="text/css"/>
             </head>
+            <header>
+	            <p>WishList</p>
+            </header>
             <body>
                 <!-- page web -->
                 <div class="container">
                     $content
                 </div>
-                
-                <!-- js boostrap -->
-                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
             </body>
+            <footer>
+                <p><a href={$this->container->router->pathFor('accueil')}>Retour a l'accueil</a></p>
+                <p>Auteurs: Fabrice ARNOUT, Guillaume RENARD, Marcus RICHIER, Mathieu VINOT, Lucas WEISS</p>                    
+            </footer>
         </html> 
 END;
     }
@@ -57,18 +61,29 @@ END;
      */
     public function affichageAccueil() {
         return <<<END
-            <div class="text-page-accueil">
+            <div class="text-accueil">
                 <p>
                     Bonjour, bienvenue sur notre projet PHP de 3eme semestre. <br>
                     Crée votre liste et accès à vos listes et réserver les items !<br>
                     Où encore modifier vos items de vos listes pour créer des superbes listes.<br>
                     <br>
-                    Auteurs: Fabrice ARNOUT, Guillaume RENARD, Marcus RICHIER, Mathieu VINOT, Lucas WEISS                    
+                    <p><a href={$this->container->router->pathFor('creationCompte')}>Se creer un compte</a></p>
+                    <p><a href={$this->container->router->pathFor('affichageListesPublique')}>Afficher toutes les listes publiques</a></p>
+                    <p><a href={$this->container->router->pathFor('creationListe')}>Creer nouvelles listes</a></p>
+
                 </p>
             </div>
 END;
     }
 
+    public function htmlErreur($message) {
+        return <<<END
+            <div class="text-erreur">
+                <p>$message</p>
+                <p><a href={$this->container->router->pathFor('accueil')}>Retour a l'accueil</a></p>
+            </div>
+END;
+    }
 
 
 }
